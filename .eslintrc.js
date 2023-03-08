3a8118aa68aa6bc6768194549a3fe09a0394ad9b
seho <seho.zaq@gmail.com>
2023-03-08 23:05:22 +0800

    🔧 build: optimization type check toolchain

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    // 支持ts-ignore
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
