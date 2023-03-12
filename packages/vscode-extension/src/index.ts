5c6107e5abc4dc3271720f19551e8d3273dc87f8
seho <seho.zaq@gmail.com>
2023-03-12 22:37:38 +0800

    feat(vscode-extension): bind command logic

--- packages/vscode-extension/src/index.ts
+++ packages/vscode-extension/src/index.ts
@@ -1,7 +1,26 @@
-import { window } from 'vscode';
+import { window, commands, extensions } from 'vscode';
+import { sendMessage } from '@swordjs/stt-core';
+import type { GitExtension } from '../typings/git';
+
+function getGitExtension() {
+  return extensions.getExtension<GitExtension>('vscode.git')?.exports;
+}
 
 export function activate() {
-  window.showInformationMessage('Hello');
+  // 绑定command
+  commands.registerCommand('extension.translateGitMessage', () => {
+    const gitExtension = getGitExtension();
+
+    if (!gitExtension?.enabled) {
+      window.showErrorMessage('Git extensions are not currently enabled, please try again after enabled!');
+      return false;
+    }
+
+    const repo = gitExtension.getAPI(1).repositories[0];
+    sendMessage(repo.inputBox.value);
+    // 显示信息
+    window.showInformationMessage(`Hello World! ${repo.inputBox.value}`);
+  });
 }
 
 export function deactivate() {
