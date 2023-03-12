d0d3c33da8427fd14e547c32d9a9cc0d1d61d03d
seho <seho.zaq@gmail.com>
2023-03-12 22:40:02 +0800

    ✨ feat: debug sendMessage

--- packages/core/src/translate.ts
+++ packages/core/src/translate.ts
@@ -1,11 +1,26 @@
-import { ChatGPTAPI } from 'chatgpt';
+import { ChatGPTAPI } from '@swordjs/chatgpt';
+import { loadPromptTemplate } from '@swordjs/prompt-template';
+import path from 'path';
 import { ref } from '@vue/reactivity';
+import type { ChatCompletionRequestMessage } from '#types/openapi';
 
 export const chatgptInstance = new ChatGPTAPI({
-  apiKey: process.env.PUBLIC_API_KEY
+  apiKey: process.env.PUBLIC_API_KEY as string
 });
 
+let template: { messages: ChatCompletionRequestMessage[] } | undefined;
+
 export const sendMessage = async (message: string) => {
-  const res = await chatgptInstance.sendMessage(message);
-  return res;
+  if (!template) {
+    template = await loadPromptTemplate(path.resolve(__dirname, '../prompt/default.md'));
+  }
+  console.log([
+    ...template.messages,
+    {
+      role: 'user',
+      content: message
+    }
+  ]);
+  // const res = await chatgptInstance.sendMessage(message);
+  // return res;
 };
