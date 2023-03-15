61c9dd8cb51eb7de770ed3ff2d56dcab3c05ad62
seho <seho.zaq@gmail.com>
2023-03-15 23:50:56 +0800

    ✨ feat(website): add main page

---
+++ packages/website/src/components/Input.svelte
@@ -0,0 +1,19 @@
+<div class="input relative flex-1 h-42vw">
+  <div class="select-file absolute w-318px h-68px text-#1A237E rounded-10px flex justify-center items-center cursor-pointer text-24px">Select File (.pdf, .docx)</div>
+</div>
+
+<style>
+  .input {
+    background: rgba(217, 217, 217, 0.68);
+    backdrop-filter: blur(5px);
+    border-radius: 5px;
+  }
+  .select-file {
+    border: 2px dashed #1a237e;
+    font-family: 'Inter';
+    font-style: italic;
+    bottom: 97px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+</style>
---
+++ packages/website/src/components/Output.svelte
@@ -0,0 +1,9 @@
+<div class="output flex-1 h-42vw ml-14px"></div>
+
+<style>
+  .output {
+    background: rgba(217, 217, 217, 0.68);
+    backdrop-filter: blur(5px);
+    border-radius: 5px;
+  }
+</style>
